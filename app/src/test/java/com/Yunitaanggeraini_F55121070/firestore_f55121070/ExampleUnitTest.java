2d71fed95ba13ae2b53da9f8a597b73ab8415c04
Acer <budiagungfajariyanto@gmail.com>
2022-11-20 17:46:03 +0700

    Initial commit

---
+++ app/src/test/java/com/Yunitaanggeraini_F55121070/firestore_f55121070/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.Yunitaanggeraini_F55121070.firestore_f55121070;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
